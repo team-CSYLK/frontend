19156ab445b475cc9aeabcf3d77bd9532edb9e22
SUHONG\topic <topic828@Naver.com>
2023-01-25 20:31:12 +0900

    초기세팅

---
+++ public/components/MessageButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessageButton = () => {
+  return (
+    <div>MessageButton</div>
+  )
+}
+
+export default MessageButton
---
+++ public/redux/config/configStore.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// import comments from "../modules/commentsSlice";
+
+const store = configureStore({
+  reducer: {
+    // comments,
+  },
+});
+
+export default store;

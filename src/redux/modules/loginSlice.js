52bc0ebaad60ab1d585cd0733d3400f3ac395b6a
SUHONG\topic <topic828@Naver.com>
2023-01-31 05:39:25 +0900

    [feat] 닉네임 useState로 입력, 유효성 검사 추가 , 버튼 diabled기능 추가

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -25,18 +25,21 @@ export const __postLogin = createAsyncThunk(
         });
       if (data.status === 200) {
         // alert("로그인 성공");
-        console.log(data.data.nickname);
-        if (data.data.nickname === undefined) {
-          window.location.href = "SignNick";
+        console.log(data);
+        console.log(data.data);
+        // console.log(data.data.nickname);
+        if (data.data === "") {
+          window.location.href = "/SignNick";
         } else {
           sessionStorage.setItem("nickname", data.data);
+          window.location.href = "/Main";
         }
       }
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
-      //   if (error.response.status === 409) {
-      //     alert("아이디와 패스워드를 확인해주세요");
-      //   }
+      // if (error.response.status === 409) {
+      //   alert("아이디와 패스워드를 확인해주세요");
+      // }
       return thunkAPI.rejectWithValue(error);
     }
   }
@@ -52,11 +55,15 @@ export const __addNick = createAsyncThunk(
         .then((res) => {
           return res;
         });
-      //   if (data.status === 200) {
-      //     sessionStorage.setItem("nickname", data.data);
-      //   }
+      if (data.status === 200) {
+        sessionStorage.setItem("nickname", payload.nickname);
+        window.location.href = "/Main";
+      }
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
+      if (error.response.status === 409) {
+        alert("중복된 닉네임입니다.");
+      }
       return thunkAPI.rejectWithValue(error);
     }
   }
